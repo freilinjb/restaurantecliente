b9f7f518ef59485874bd93bbbbbcd6759d3507c1
freilinjb <freilinjb@gmail.com>
2020-12-29 23:08:14 -0400

    Eslitos al proyecto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
+import React from "react";
+import { Routes, Route } from "react-router";
 
-import Ordenes from './components/paginas/Ordenes';
-import Menu from './components/paginas/Menu';
-import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+import Ordenes from "./components/paginas/Ordenes";
+import Menu from "./components/paginas/Menu";
+import NuevoPlatillo from "./components/paginas/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
     <div>
+      <Sidebar/>
+
       <Routes>
-        <Route path="/" element={<Ordenes/>} />
-        <Route path="/Menu" element={<Menu/>} />
-        <Route path="/NuevoPlatillo" element={<NuevoPlatillo/>} />
+        <Route path="/" element={<Ordenes />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/NuevoPlatillo" element={<NuevoPlatillo />} />
       </Routes>
     </div>
   );
