1ba116eb39becd6bf0d39dfc75beda8b3d9f820c
freilinjb <freilinjb@gmail.com>
2020-12-30 21:00:08 -0400

    Añadiendo formik al formulario

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useFormik } from  'formik';
 
 const NuevoPlatillo = () => {
+    //validacion y leer los datos del formulario
+    const formik = useFormik({
+        initialValues: {
+            nombre: 'Freilin JOSE',
+            precio: '',
+            categoria: '',
+            descripcion: '',
+        },
+        onSubmit: datos => {
+            console.log(datos);
+        }
+    });
+
     return ( 
         <>
             <h1 className="text-3xl font-light md-4">Nuevo Platillo</h1>
             <div className="flex justify-center mt-10">
                 <div className="w-full max-w-3xl">
-                    <form>
+                    <form
+                        onSubmit={formik.handleSubmit}
+                    >
                         <div className="mb-4">
                             <label htmlFor="nombre" className="block text-gray-700 text-sm font-bold mb-2">Nombre</label>
                             <input type="text" 
@@ -14,6 +30,8 @@ const NuevoPlatillo = () => {
                                     id="nombre" 
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     placeholder="Nombrer Platillo"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
                             />
                         </div>
 
@@ -25,6 +43,8 @@ const NuevoPlatillo = () => {
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     placeholder="$20"
                                     min="0"
+                                    value={formik.values.precio}
+                                    onChange={formik.handleChange}
                             />
                         </div>
 
@@ -34,6 +54,8 @@ const NuevoPlatillo = () => {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="categoria"
                                 name="categoria"
+                                value={formik.values.categoria}
+                                    onChange={formik.handleChange}
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -48,20 +70,24 @@ const NuevoPlatillo = () => {
                         <div className="mb-4">
                             <label htmlFor="imagen" className="block text-gray-700 text-sm font-bold mb-2">Imagen</label>
                             <input type="file" 
-                                    id="precio" 
-                                    name="precio"
+                                    id="imagen" 
+                                    name="imagen"
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    value={formik.values.imagen}
+                                    onChange={formik.handleChange}
                             />
                         </div>
 
                         <div className="mb-4">
                             <label htmlFor="descripcion" className="block text-gray-700 text-sm font-bold mb-2">Descripcion</label>
                             <textarea 
-                                type="text" 
+                                type="text"  
                                 name="descripcion"
                                 id="descripcion" 
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
                                 placeholder="Descripcion del platillo"
+                                value={formik.values.descripcion}
+                                onChange={formik.handleChange}
                             ></textarea>
                         </div>
 
