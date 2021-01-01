c4810d7e1d01a69ccd893657e8d30a590f7c7ea4
freilinjb <freilinjb@gmail.com>
2020-12-31 22:45:46 -0400

    Mostrando platillo en la infterfaz

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -2,6 +2,8 @@ import React,{ useState, useEffect, useContext } from 'react';
 import  { Link } from 'react-router-dom';
 import { FirebaseContext } from '../../firebase';
 
+import Platillo from  '../ui/Platillo';
+
 const Menu = () => {
     //definir el state para los platillos
     const [platillos, setPlatillos] = useState([]);
@@ -38,6 +40,13 @@ const Menu = () => {
             <Link to="/NuevoPlatillo" className="bg-blue-800 hover:bg-blue-700 inline-block mb-5 p-2 text-white uppercase font-bold">
                 Agregar Platillo
             </Link>
+
+            {platillos.map(platillo => (
+                <Platillo 
+                    key={platillo.id}
+                    platillo={platillo}
+                />
+            ))}
         </>
      );
 }
---
+++ src/components/ui/Platillo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Platillo = ({platillo}) => {
+    const { nombre, imagen } = platillo; 
+
+    return ( 
+        <div className="w-full px-3 md-4">
+            <div className="p-5 shadow-md bg-white">
+                <div className="lg:flex">
+                    <div className="lg:w-5/12 xl:w-3/12">
+                        <img src={imagen} alt="Imagen del platillo"/>
+                    </div>
+                    <div className="lg:w-7/12 xl:w-9/12">
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Platillo;
