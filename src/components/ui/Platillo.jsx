2cb35b88ea38054e4b108e0a0b55701395e1dbfa
freilinjb <freilinjb@gmail.com>
2020-12-31 22:51:09 -0400

    Mostrando el resto de la informacion

--- src/components/ui/Platillo.jsx
+++ src/components/ui/Platillo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Platillo = ({platillo}) => {
-    const { nombre, imagen } = platillo; 
+    const { nombre, imagen, existencia, categoria, descripcion, precio } = platillo; 
 
     return ( 
         <div className="w-full px-3 md-4">
@@ -11,7 +11,16 @@ const Platillo = ({platillo}) => {
                         <img src={imagen} alt="Imagen del platillo"/>
                     </div>
                     <div className="lg:w-7/12 xl:w-9/12">
-                        2
+                        <p className="font-bold text-2xl text-yellow-600 md-4">{nombre}</p>
+                        <p className="text-gray-600 md-4">Categoria: {' '}
+                            <span className="text-gray-700 font-bold">{categoria.toUpperCase()}</span>
+                        </p>
+                        <p className="text-gray-600 md-4">{descripcion}</p>
+
+                        <p className="text-gray-600 md-4">Preicio: {' '}
+                            <span className="text-gray-700 font-bold"> ${precio}</span>
+                        </p>
+
                     </div>
                 </div>
             </div>
