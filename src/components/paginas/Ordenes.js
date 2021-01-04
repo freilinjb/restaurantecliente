2fe2d3f1f0c61ae4c29bf9367720a905a3cc10ba
freilinjb <freilinjb@gmail.com>
2021-01-04 18:57:00 -0400

    Obtiniendo las ordenes que no esten completas -- TERCERA PARTE CON ANDROID

--- src/components/paginas/Ordenes.js
+++ src/components/paginas/Ordenes.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState, useContext} from 'react';
+import { FirebaseContext } from '../../firebase';
+
 
 const Ordenes = () => {
+
+    //Context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    const [ordenes, setOrdenes] = useState([]);
+
+    useEffect(() => {
+        const obtenerOrdenes = () => {
+            firebase.db.collection('ordenes').where('completado', "==", false).onSnapshot(manejarSnapshot);
+        }
+        obtenerOrdenes();
+    }, []);
+
+    function manejarSnapshot(snapshot) {
+        const ordenes = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+        console.log('ordenes: ', ordenes);
+    }
+
+
     return ( 
         <>
             <h1 className="text-3xl font-light md-4">Ordenes</h1>
